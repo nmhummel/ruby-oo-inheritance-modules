7bd5566e2b199b5cb5cf025d39703d9663ca1d1e
nmhummel <natalie.hummel@gmail.com>
2020-11-07 10:15:42 -0600

    commit tree c7412c60f6033975b75b9ba93a9df61a19689934

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,19 @@
-require_relative './class_methods_module.rb'
-require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+    extend FancyDance::ClassMethods      # extend denotes Class method borrowing
+    include FancyDance::InstanceMethods     #include denotes instance method borrowing
+
+    # We refer to the name-spaced modules or classes with ::. 
+    # This syntax references the parent and child relationship of the nested modules.
+    
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+
+
+end
+
